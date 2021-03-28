4271e6b477e722c22305f2b2298f0f98e3f6fb7d
mmmccormack <pmccorma@gmail.com>
2021-03-28 19:51:38 -0400

    some more JS functionality and CSS zhuzhing :sparkles:

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const playlistApp = {};
 
 // this function takes in the artist name and country selected. The artist name is used to determine the artist's unique MusicBrainz ID number, which can be passed to the next API. The country parameter
 
-playlistApp.getArtistId = (artist, country) => {
+playlistApp.getArtistId = (artist, country, popularity) => {
     
+    playlistApp.playlistBox.classList.remove('off');
+    playlistApp.playlistBox.textContent = "Loading playlist...";
+    playlistApp.playlistBox.classList.add('loading');
     // this is a fetch request to return an artist's mbID number, which is necessary to complete the musicovery API request.
     const url = new URL('http://musicbrainz.org/ws/2/artist');
     // this api defaults to XML delivery - the 'fmt: json' is necessary to receive the data in JSON format. 
@@ -21,7 +24,7 @@ playlistApp.getArtistId = (artist, country) => {
         }).then( (data) => {
             // this data point gathers the MusicBrainz ID number for the artist, which is passed to the musicovery API in the next function to generate the playlist.
             const artistId = data.artists[0].id;
-            playlistApp.getPlaylist(artistId, country);
+            playlistApp.getPlaylist(artistId, country, popularity);
         });
 }
 
@@ -30,7 +33,8 @@ playlistApp.getArtistId = (artist, country) => {
 
 // this function takes the artist ID number from the previous function, as well as any other properties defined by the user in th UI, and uses the musicovery API to generate a list of 12 similar artists and songs based on the user input values.
 
-playlistApp.getPlaylist = (artistId, country) => {
+playlistApp.getPlaylist = (artistId, country, popularity) => {
+    
     // API call - to avoid a CORS error, a call to the Juno proxy server must be used. 
     axios({
         method:'GET',
@@ -41,20 +45,23 @@ playlistApp.getPlaylist = (artistId, country) => {
             reqUrl: `http://musicovery.com/api/V6/playlist.php?&fct=getfromartist&artistmbid=${artistId}&focusera=true`,
             listenercountry: country,
             obscureartists: true,
-            focusgenre: false
+            focusgenre: false,
+            popularitymin: popularity
             }
         }).then((res) => {
             // this data point correlates to the array of track objects, which we will need to extract from to display our playlist.
             const playlist = res.data.tracks.track;
+            playlistApp.playlistBox.textContent = "";
             // add a for each statement here to extract artist name and song title, create a list element for artist and song, and append that list item to the unordered list.
             playlist.forEach(function(track) {
                 const artistName = track.artist_display_name; 
                 const trackTitle = track.title;
                 const ulElement = document.querySelector('ul');
                 const listElement = document.createElement('li');
-                listElement.textContent = `${artistName} - ${trackTitle}`
+                listElement.textContent = `${artistName} ● ${trackTitle}`
                 ulElement.appendChild(listElement);
             })
+            playlistApp.playlistBox.classList.remove('loading');
         })
         .catch(err => { console.log('error')})
 }
@@ -62,10 +69,12 @@ playlistApp.getPlaylist = (artistId, country) => {
 // app initialize function - declaring global variables, event listener for button, and for in loop to populate dropdown list of countries
 
 playlistApp.init = function() {
-    playlistApp.artistInput = document.querySelector('input');
+    playlistApp.artistInput = document.querySelector('.artistName');
     playlistApp.countryCodes = document.getElementById('countryCodes');
     playlistApp.submitButton = document.querySelector('button');
     playlistApp.count = 0;
+    playlistApp.playlistBox = document.querySelector('.playlist');
+    playlistApp.popularityKnob = document.getElementById('popularity');
 
     for (country in countries) {
         const listOption = document.createElement('option');
@@ -78,60 +87,20 @@ playlistApp.init = function() {
     playlistApp.submitButton.addEventListener('click', function() {
         const artist = playlistApp.artistInput.value;
         const country = playlistApp.countryCodes.value;
-        playlistApp.getArtistId(artist, country);
+        const popularity = playlistApp.popularityKnob.value;
+        playlistApp.getArtistId(artist, country, popularity);
     })
 
+    playlistApp.rotate = (value) => {
+        document.querySelector('.knob').style.transform=`rotate(${value * 1.8 }deg)`;
+         document.getElementById('popularity').value = value;
+    }
+
 }
+
+
 
 playlistApp.init();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function getReleaseId(artistName, trackTitle) {
-
-//     axios({
-//             method:'GET',
-//             url: `http://musicbrainz.org/ws/2/release/?query=release:${trackTitle}+artist:${artistName}`,
-//             // http://musicbrainz.org/ws/2/release/?query=release:breathe+artist:kylie+minogue
-//             fmt: 'json',
-//         })
-//         .then(function(result) {
-//             // this gets the release mbid number needed for the search in the next API. 
-//             const artId = result.data.releases[2].id;
-//             getArt(artId)
-
-//         });
-
-//         }
-
-
-
-
-// function getArt(artistMbId) {
-
-//             axios({
-//                 method:'GET',
-//                 url: `http://coverartarchive.org/release/${artistMbId}`,
-//                 responseType: 'json',
-//             })
-//             .then(function(result) {
-//                 // this gets the release mbid number needed for the search in the next API. 
-//                 console.log(result.data.images[0].thumbnails['250']);
-//                 const divElement = document.querySelector('.box');
-//                 const imgElement = document.createElement('img');
-//                 imgElement.src = result.data.images[0].thumbnails['250'];
-//                 divElement.appendChild(imgElement);
-//             });
-//         }
